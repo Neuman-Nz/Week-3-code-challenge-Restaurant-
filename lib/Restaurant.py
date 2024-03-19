b282a87995b89e429fce8f63a662f11b75d7297f
neuman <owiroadams@gmail.com>
2024-03-19 10:32:24 +0300

    Started coding

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -8,3 +8,14 @@ class Restaurant:
     
     def add_review(self, review):
         self.reviews.append(review)
+        
+    def customers(self):
+        return list(set([review.customer() for review in self.reviews]))
+    
+    def average_star_rating(self):
+        if not self.reviews:
+            return 0
+        total_ratings = sum([review.rating() for review in self.reviews])
+        return total_ratings / len(self.reviews)
+    
+        
